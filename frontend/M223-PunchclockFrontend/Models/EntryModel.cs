ecc4eee0c439c3ab4f0db5e2b2c7ce40553a0a3c
speyck <54800864+speyck@users.noreply.github.com>
2021-09-22 11:35:41 +0200

    Create EntryModel.cs

---
+++ frontend/M223-PunchclockFrontend/Models/EntryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace M223_PunchclockFrontend.Models
+{
+    public class EntryModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("checkIn")]
+        public DateTime CheckIn { get; set; }
+
+        [JsonProperty("checkOut")]
+        public DateTime CheckOut { get; set; }
+    }
+}
