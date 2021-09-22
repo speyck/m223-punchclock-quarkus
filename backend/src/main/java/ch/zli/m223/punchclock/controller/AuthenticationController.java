1898154108a132d4c7dc503192e1e952f22b78a9
speyck <54800864+speyck@users.noreply.github.com>
2021-09-22 16:39:11 +0200

    Added checkIfUserExists and implemented it

--- backend/src/main/java/ch/zli/m223/punchclock/controller/AuthenticationController.java
+++ backend/src/main/java/ch/zli/m223/punchclock/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package ch.zli.m223.punchclock.controller;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import ch.zli.m223.punchclock.domain.User;
 import ch.zli.m223.punchclock.service.AuthenticationService;
@@ -18,7 +19,9 @@ public class AuthenticationController {
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
     public String login(User user) {
-        // todo heck if user exists, if not then throw NotAutherizedException
+        if (!authService.checkIfUserExists(user)) {
+            throw new NotAuthorizedException("User \'" + user.getUsername() + "\' with password \'" + user.getPassword() + "\' could not be authenticated" , Response.Status.UNAUTHORIZED);
+        }
 
         return authService.GenerateValidJwtToken(user.getUsername());
     }
--- backend/src/main/java/ch/zli/m223/punchclock/service/AuthenticationService.java
+++ backend/src/main/java/ch/zli/m223/punchclock/service/AuthenticationService.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.Query;
+import javax.persistence.EntityManager;
 
 import io.smallrye.jwt.build.Jwt;
 import org.eclipse.microprofile.jwt.Claims;
 
+import ch.zli.m223.punchclock.domain.User;
+
 @RequestScoped
 public class AuthenticationService {
+
+    @Inject
+    private EntityManager entityManager;
     
     public String GenerateValidJwtToken(String username){
         String token =
@@ -21,6 +29,14 @@ public class AuthenticationService {
             .expiresIn(Duration.ofHours(1)) 
             .sign();
         return token;
+    }
+
+    public boolean checkIfUserExists(User user) {
+        Query stmt = entityManager.createQuery("SELECT COUNT(*) FROM User WHERE username = :name AND password = :password");
+        stmt.setParameter("name", user.getUsername());
+        stmt.setParameter("password", user.getPassword());
+
+        return (long)stmt.getSingleResult() == 1;
     }
 
 }
