23573edec5e9ad58623cdd41a875432445de4c1c
speyck <54800864+speyck@users.noreply.github.com>
2021-09-22 11:37:12 +0200

    Create CategoryModel.cs

---
+++ frontend/M223-PunchclockFrontend/Models/CategoryModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M223_PunchclockFrontend.Models
+{
+    public class CategoryModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("entries")]
+        public List<EntryModel> Entries { get; set; }
+    }
+}
