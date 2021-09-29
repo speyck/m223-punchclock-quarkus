4380cdf04e1863d81403af204df86684b0bb5fde
speyck <54800864+speyck@users.noreply.github.com>
2021-09-29 11:27:32 +0200

    Create Participant.java

---
+++ backend/src/main/java/ch/zli/m223/punchclock/domain/Participant.java
@@ -0,0 +1,73 @@
+package ch.zli.m223.punchclock.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = true)
+    private String phone;
+
+    @ManyToOne
+    private Entry entry;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Entry getEntry() {
+        return entry;
+    }
+
+    public void setEntry(Entry entry) {
+        this.entry = entry;
+    }
+}
