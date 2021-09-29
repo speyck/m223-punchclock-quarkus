e930926febf31e87717f0e11b5185fa906b3fdda
speyck <54800864+speyck@users.noreply.github.com>
2021-09-29 13:10:44 +0200

    Create Location.java

---
+++ backend/src/main/java/ch/zli/m223/punchclock/domain/Location.java
@@ -0,0 +1,64 @@
+package ch.zli.m223.punchclock.domain;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Integer zip;
+
+    @Column(nullable = false)
+    private String city;
+
+    @OneToMany(mappedBy = "location")
+    private List<Entry> entries;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<Entry> entries) {
+        this.entries = entries;
+    }
+}
