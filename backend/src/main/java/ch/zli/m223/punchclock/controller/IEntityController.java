ed7fbe1305637a8b9f423f9513f2e2a6a92b703e
speyck <54800864+speyck@users.noreply.github.com>
2021-09-29 11:38:58 +0200

    Create IEntityController.java

---
+++ backend/src/main/java/ch/zli/m223/punchclock/controller/IEntityController.java
@@ -0,0 +1,7 @@
+package ch.zli.m223.punchclock.controller;
+
+import ch.zli.m223.punchclock.ICreateReadUpdateDelete;
+
+public interface IEntityController<T> extends ICreateReadUpdateDelete<T> {
+
+}
