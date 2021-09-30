082f4f8fd9a06269dee2b470bc32f88ea7bbc8a5
speyck <54800864+speyck@users.noreply.github.com>
2021-09-30 15:32:23 +0200

    Add starting data in import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,10 @@
 INSERT INTO category (title) VALUES ('Admin');
 INSERT INTO category (title) VALUES ('Projekt');
 INSERT INTO category (title) VALUES ('IT-Support');
+
+INSERT INTO location (address, zip, city) VALUES ('Bahnhofsstrasse 14', 7000, 'Chur');
+INSERT INTO location (address, zip, city) VALUES ('Postplatz 2', 7432, 'Zillis');
+INSERT INTO location (address, zip, city) VALUES ('Musterstrasse 101', 8000, 'Zürich');
+
+
+INSERT INTO user(username, password) VALUES ('zli', 'root');
