fae720ed021f375e820ee1012bee12911352cfc8
speyck <54800864+speyck@users.noreply.github.com>
2021-09-30 15:37:07 +0200

    Create LocationModel

---
+++ frontend/M223-PunchclockFrontend/Models/LocationModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace M223_PunchclockFrontend.Models
+{
+    public class LocationModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("zip")]
+        public int Zip { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("entries")]
+        public List<EntryModel> Entries { get; set; }
+    }
+}
