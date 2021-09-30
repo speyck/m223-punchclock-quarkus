df1ad84222c4efee0696cc4739e81ae299eaeec0
speyck <54800864+speyck@users.noreply.github.com>
2021-09-30 15:38:18 +0200

    Create Authentication- & AuthorizationUserModel

---
+++ frontend/M223-PunchclockFrontend/Models/AuthenticationUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace M223_PunchclockFrontend.Models
+{
+    public class AuthenticationUserModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ frontend/M223-PunchclockFrontend/Models/AuthorizationUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M223_PunchclockFrontend.Models
+{
+    public class AuthorizationUserModel
+    {
+        public string Token { get; set; }
+        public string Username { get; set; }
+    }
+}
