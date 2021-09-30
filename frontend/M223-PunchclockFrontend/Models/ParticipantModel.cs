8d5e681c0d00d352c244df9c0322a1f30facd23d
speyck <54800864+speyck@users.noreply.github.com>
2021-09-30 15:37:21 +0200

    Create ParticipantModel

---
+++ frontend/M223-PunchclockFrontend/Models/ParticipantModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace M223_PunchclockFrontend.Models
+{
+    public class ParticipantModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("firstname")]
+        public string Firstname { get; set; }
+
+        [JsonProperty("lastname")]
+        public string Lastname { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("pone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("entry")]
+        public EntryModel Entry { get; set; }
+    }
+}
